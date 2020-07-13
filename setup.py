43175d7381a489b0f45eaeb4725114181866642b
butyr <L.Butyrev@gmx.de>
2020-07-13 14:06:07 +0200

    added setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pytorch-transformer-butyr",
+    name="pytorch_transformer_butyr",
     version="0.0.1",
     author="Leonid Butyrev",
     author_email="L.Butyrev@gmx.de",
-    description="pytorch transformer implementation",
+    description="PyTorch transformer implementation",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/butyr/pytorch-transformer",
