f5f65bbc975a8886d0f002e953b1f8a443325ae6
butyr <L.Butyrev@gmx.de>
2020-07-14 11:36:40 +0200

    removed unused unittest

--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -92,9 +92,6 @@ class TestTensorShapes(unittest.TestCase):
 
         self.assertEqual((batch_size, sent_len, model_dim), dec.shape)
 
-    def test_right_shift(self):
-        pass
-
 
 class TestEmbedding(unittest.TestCase):
 
